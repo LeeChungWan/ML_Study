af8f6c6d4ab4f72f2e1c49a24f09a8b9537fa02b
leechungwan <cnddhks846@naver.com>
2018-07-18 18:29:59 +0900

    update MinMaxScaler Function

--- lab07-1/02.linear_regression_without_min_max.py
+++ lab07-1/02.linear_regression_without_min_max.py
@@ -1,6 +1,14 @@
 import tensorflow as tf
 import numpy as np
 
+
+def MinMaxScaler(data):
+    numerator = data - np.min(data, 0)
+    denominator = np.max(data, 0) - np.min(data, 0)
+    # noise term prevents the zero division
+    return numerator / (denominator + 1e-7)
+
+
 xy = np.array([[828.659973, 833.450012, 908100, 828.349976, 831.659973],
                [823.02002, 828.070007, 1828100, 821.655029, 828.070007],
                [819.929993, 824.400024, 1438100, 818.97998, 824.159973],
@@ -9,10 +17,12 @@ xy = np.array([[828.659973, 833.450012, 908100, 828.349976, 831.659973],
                [819, 823, 1198100, 816, 820.450012],
                [811.700012, 815.25, 1098100, 809.780029, 813.669983],
                [809.51001, 816.659973, 1398100, 804.539978, 809.559998]])
-# 데이터의 형태가 들숙 날숙 할때 nomalize 필수!!
+# 데이터의 형태가 들숙 날숙 할때 normalize 필수!!
+xy = MinMaxScaler(xy)
+"""
 for i in range(xy.shape[1]):
     xy[:, i] = (xy[:, i] - xy[:, i].mean()) / xy[:, i].std()
-
+"""
 x_data = xy[:, 0:-1]
 y_data = xy[:, [-1]]
 
