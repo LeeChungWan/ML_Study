a570e17610f795cabf0296b5cadd54814f8fe119
leechungwan <cnddhks846@naver.com>
2018-08-02 15:15:52 +0900

    update 01.captureAndSaveImg.py

--- openCV/01.captureAndSaveImg.py
+++ openCV/01.captureAndSaveImg.py
@@ -1,4 +1,5 @@
 import cv2
+from time import sleep
 
 CAM_ID = 0
 
@@ -8,13 +9,17 @@ def capture(camid=CAM_ID):
     if cam.isOpened() == False:
         print('cant open the cam (%d)' % camid)
         return None
-
-    ret, frame = cam.read()
-    if frame is None:
-        print('frame is not exist')
-        return None
-
-    cv2.imwrite('messigray.png', frame, params=[cv2.IMWRITE_PNG_COMPRESSION, 0])
+    # range(N) N으로 사진 갯수 조정
+    for i in range(20):
+        file_name = str(i) + '.png'
+        file_path = './data/' + file_name
+        ret, frame = cam.read()
+        if frame is None:
+            print('frame is not exist')
+            return None
+        cv2.imwrite(file_path, frame, params=[cv2.IMWRITE_PNG_COMPRESSION, 0])
+        # sleep(N) N으로 시간 간격으로 사진을 찍는다.
+        sleep(2)
     cam.release()
 
 
