bf8cac63eb54fa5bc5a5b55f34ca4b91c7c360a4
leechungwan <cnddhks846@naver.com>
2018-08-04 00:34:45 +0900

    added two function (showScreenAndDetectFace and refreshScreen)

--- openCV/DDD/webcam_detect.py
+++ openCV/DDD/webcam_detect.py
@@ -1,32 +1,29 @@
 import cv2
 import sys
 import time
-
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-cap = cv2.VideoCapture(0)
+import detection_utilities as du
 
 windowName = 'Webcam Screen'
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        roi_gray = gray[y:y + h, x:x + w]
-        roi_color = frame[y:y + h, x:x + w]
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
-
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
+# while True:
+#     # Capture frame-by-frame
+#     ret, frame = cap.read()
+#
+#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#
+#     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+#     for (x, y, w, h) in faces:
+#         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+#         roi_gray = gray[y:y + h, x:x + w]
+#         roi_color = frame[y:y + h, x:x + w]
+#         eyes = eye_cascade.detectMultiScale(roi_gray)
+#
+#         for (ex, ey, ew, eh) in eyes:
+#             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+#
+#     cv2.imshow('frame', frame)
+#     if cv2.waitKey(20) & 0xFF == ord('q'):
+#         break
 
 
 def getCameraStreaming():
@@ -44,15 +41,32 @@ def setDefaultCameraSetting():
     cv2.setWindowProperty(winname=windowName, prop_id=cv2.WINDOW_FULLSCREEN, prop_value=cv2.WINDOW_FULLSCREEN)
 
 
+def showScreenAndDetectFace(capture):
+    while True:
+        ret, frame = capture.read()
+        face_coordinates = du.getFaceCoordinates(frame)
+        refreshScreen(frame, face_coordinates)
+
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
+
+
+def refreshScreen(frame, face_coordinates):
+    if face_coordinates is not None:
+        du.drawFace(frame, face_coordinates)
+    cv2.imshow(windowName, frame)
+
+
 def main():
     print("Start main() function.")
 
     capture = getCameraStreaming()
     setDefaultCameraSetting()
+    showScreenAndDetectFace(capture)
 
 
 if __name__ == '__main__':
     main()
 
-cap.release()
+#cap.release()
 cv2.destroyAllWindows()
