5f4d0100a697d1bc126825ae9a144a4db405a170
leechungwan <cnddhks846@naver.com>
2018-08-05 21:15:47 +0900

    modified preprocessing func and added extract_features func

--- openCV/DDD/model/feature_utility.py
+++ openCV/DDD/model/feature_utility.py
@@ -5,5 +5,25 @@ import sys, os
 
 def preprocessing(img, size=(48, 48)):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = cv2.resize(img, size)
+    img = cv2.resize(img, size).astype(np.float32)
     return img
+
+
+def extract_features(path):
+    X, y = [], []
+    label = 0
+    for dirnames in os.listdir(path):
+        sub_path = os.path.join(path, dirnames)
+        for filename in os.listdir(sub_path):
+            file_path = os.path.join(sub_path, filename)
+            img = cv2.imread(file_path)
+            img = preprocessing(img)
+            X.append(img)
+
+            class_label = [0, 0]
+            class_label[label] = 1
+            y.append(class_label)
+        label += 1
+    X = np.asanyarray(X)
+    y = np.asanyarray(y)
+    return X, y
